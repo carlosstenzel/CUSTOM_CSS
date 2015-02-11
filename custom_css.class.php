f51eaa863a7e96cc8c9995c1d72d6ee6b7609694
Carlos Stenzel <betinhostenzel@hotmail.com>
2015-02-11 11:19:38 -0200

    Create custom_css.class.php

---
+++ custom_css.class.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * ------------------------------------------
+ * CUSTOM CSS
+ * Framework php para criação de css de facil
+ * customização
+ * Carlos Stenzel -> www.carlosstenzel.com
+ * CodeHamper -> www.codehamper.com
+ * -------------------------------------------
+*/
+
+class Custom_css{
+
+	/**
+	 * $nome -> nome do estilo
+	 * $atributos -> atributos
+    * $loaded -> array de estilos carregados
+   */
+   
+	private $nome;
+   private $atributos;
+	
+	public function __construct($nome){
+   	$this->nome = $nome;	
+	}
+	
+	public function __set($nome, $value){
+		$nome = str_replace('_','-', $nome);
+		$this->atributos[$nome] = $value;	
+	}
+	   
+   public function Inicio_file($file) {
+      $file= $file = fopen($file, "w");
+      $txt="/**
+ * ------------------------------------------
+ * CUSTOM CSS
+ * Framework php para criação de css de facil
+ * customização
+ * Carlos Stenzel -> www.carlosstenzel.com
+ * CodeHamper -> www.codehamper.com
+ * -------------------------------------------
+*/      
+";
+      fwrite($file,$txt);
+      fclose($file);
+   }
+    
+   public function show($file){
+         $file = fopen($file, "a");
+			fwrite($file,"{$this->nome}");
+			fwrite($file,"{\n");
+			if($this->atributos){
+				foreach($this->atributos as $name=>$value){
+					fwrite($file,"\t {$name}: {$value};\n");				
+				}			
+			}
+		   fwrite($file,"}\n");
+		   fclose($file);
+
+   }  
+      	
+
+}
